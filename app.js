d4ec6877c07c3e53ad4d55d68b2052459154b456
rehansajjadsiyal <princerehan6632@gmail.com>
2021-12-22 08:23:09 -0800

    nav

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ app.use('/cjs',express.static(__dirname+ 'public/js'))
 app.use('/css',express.static(path.join(__dirname+'node_modules/bootstrap/dist/css')))
 app.use('/js',express.static(path.join(__dirname+'node_modules/bootstrap/dist/js')))
 app.use('/jq',express.static(path.join(__dirname+'node_modules/jquery/dist')))
+app.use('/fa',express.static(path.join(__dirname+'node_modules/@fortawesome/fontawesome-free/less')))
 app.use(expressLayouts)
 app.set('layout','./layouts/full-width')
 app.set('views', path.join(__dirname, 'views'))
@@ -20,6 +21,9 @@ app.get('/', (req, res) => {
   //console.log('Hello world received a request.');
     //const target = process.env.TARGET || 'World';
   res.render('home');
+});
+app.get('/contact',(req,res)=>{
+
 });
 
 const port = process.env.PORT || 8080;
